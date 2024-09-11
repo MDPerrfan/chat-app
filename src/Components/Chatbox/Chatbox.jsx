1231ea24454bb6ed09e2730146fc8f57fe69b9e8
Mohammed Parves <mdperrfan@gmail.com>
2024-09-11 22:18:01 +0600

    updated

--- src/Components/Chatbox/Chatbox.jsx
+++ src/Components/Chatbox/Chatbox.jsx
@@ -1,57 +1,157 @@
-import React, { useContext, useEffect, useState } from 'react'
-import './Chatbox.css'
-import assests from '../../assets/assests.js'
-import { AppContext } from '../../Context/AppContext.jsx'
-import { arrayUnion, doc, getDoc, onSnapshot, updateDoc } from 'firebase/firestore'
-import { db } from '../../Config/firebase.js'
-import { toast } from 'react-toastify'
+import React, { useContext, useEffect, useState } from 'react';
+import './Chatbox.css';
+import assests from '../../assets/assests.js';
+import { AppContext } from '../../Context/AppContext.jsx';
+import { arrayUnion, doc, getDoc, onSnapshot, updateDoc } from 'firebase/firestore';
+import { db } from '../../Config/firebase.js';
+import { toast } from 'react-toastify';
+import  upload  from '../lib/upload'
 const Chatbox = () => {
-  const { userData, messageId , chatuser, messages, setMessages } = useContext(AppContext);
+  const { userData, messageId, chatuser, messages, setMessages } = useContext(AppContext);
   const [input, setInput] = useState("");
+
   const sendMessage = async () => {
     try {
-      if (input && messageId ) {
-        await updateDoc(doc(db, "messages", messageId ), {
+      if (input.trim() && messageId) {
+        // Update messages collection
+        await updateDoc(doc(db, "messages", messageId), {
           messages: arrayUnion({
             sId: userData.id,
             text: input,
-            createdAt: new Date()
-          })
-        })
+            createdAt: new Date(),
+          }),
+        });
+
         const userIDs = [chatuser.rId, userData.id];
         userIDs.forEach(async (id) => {
           const userChatRef = doc(db, "chats", id);
-          const userChatSnapshot = await getDoc(userChatRef)
+          const userChatSnapshot = await getDoc(userChatRef);
+
           if (userChatSnapshot.exists()) {
             const userChatData = userChatSnapshot.data();
-            const chatIndex = userChatData.chatsData.findIndex((c)=>c.messageId === messageId );
-            userChatData.chatsData[chatIndex].lastMessage = input.slice(0, 30);
-            userChatData.chatsData[chatIndex].updateAt = Date.now();
-            if (userChatData.chatsData[chatIndex].rId === userData.id) {
-              userChatData.chatsData[chatIndex].messageSeen = false
+
+            // Ensure chatsData exists
+            if (!userChatData.chatsData) {
+              userChatData.chatsData = [];
             }
-            await updateDoc(userChatRef,{
-              chatsData:userChatData.chatsData
-            })
+
+            const chatIndex = userChatData.chatsData.findIndex(
+              (c) => c.messageId === messageId
+            );
+
+            if (chatIndex !== -1) {
+              userChatData.chatsData[chatIndex].lastMessage = input.slice(0, 30);
+              userChatData.chatsData[chatIndex].updateAt = Date.now();
+
+              if (userChatData.chatsData[chatIndex].rId === userData.id) {
+                userChatData.chatsData[chatIndex].messageSeen = false;
+              }
+
+              await updateDoc(userChatRef, {
+                chatsData: userChatData.chatsData,
+              });
+            } else {
+              userChatData.chatsData.push({
+                messageId,
+                lastMessage: input.slice(0, 30),
+                updateAt: Date.now(),
+                rId: chatuser.rId,
+                messageSeen: false,
+              });
+
+              await updateDoc(userChatRef, {
+                chatsData: userChatData.chatsData,
+              });
+            }
           }
-        })
+        });
       }
     } catch (error) {
       toast.error(error.message);
-      
+    }
+    setInput("");
+  };
+  const sendImage = async (e) => {
+    try {
+      const fileUrl = await upload(e.target.files[0]);
+      if (fileUrl && messageId) {
+        await updateDoc(doc(db, "messages", messageId), {
+          messages: arrayUnion({
+            sId: userData.id,
+            image: fileUrl,
+            createdAt: new Date(),
+          }),
+        });
+        const userIDs = [chatuser.rId, userData.id];
+        userIDs.forEach(async (id) => {
+          const userChatRef = doc(db, "chats", id);
+          const userChatSnapshot = await getDoc(userChatRef);
+
+          if (userChatSnapshot.exists()) {
+            const userChatData = userChatSnapshot.data();
+
+            // Ensure chatsData exists
+            if (!userChatData.chatsData) {
+              userChatData.chatsData = [];
+            }
+
+            const chatIndex = userChatData.chatsData.findIndex(
+              (c) => c.messageId === messageId
+            );
+
+            if (chatIndex !== -1) {
+              userChatData.chatsData[chatIndex].lastMessage = "image";
+              userChatData.chatsData[chatIndex].updateAt = Date.now();
+
+              if (userChatData.chatsData[chatIndex].rId === userData.id) {
+                userChatData.chatsData[chatIndex].messageSeen = false;
+              }
+
+              await updateDoc(userChatRef, {
+                chatsData: userChatData.chatsData,
+              });
+            } else {
+              userChatData.chatsData.push({
+                messageId,
+                lastMessage: input.slice(0, 30),
+                updateAt: Date.now(),
+                rId: chatuser.rId,
+                messageSeen: false,
+              });
+
+              await updateDoc(userChatRef, {
+                chatsData: userChatData.chatsData,
+              });
+            }
+          }
+        });
+      }
+    } catch (error) {
+      toast.error(error.message)
+    }
+  }
+  const convertTimeStamp = (timestamp) => {
+    let date = timestamp.toDate();
+    const hour = date.getHours();
+    const minute = date.getMinutes();
+    if (hour > 12) {
+      return hour - 12 + ":" + minute + "PM"
+    } else {
+      return hour + ":" + minute + "AM"
     }
   }
   useEffect(() => {
-    if (messageId ) {
-      const unSub = onSnapshot(doc(db, "messages", messageId ), (res) => {
-        setMessages(res.data().messages.reverse());
-        console.log(res.data().messages.reverse())
-      })
+    if (messageId) {
+      const unSub = onSnapshot(doc(db, "messages", messageId), (res) => {
+        const fetchedMessages = res.data()?.messages || [];
+        setMessages(fetchedMessages.reverse());
+      });
       return () => {
         unSub();
-      }
+      };
     }
-  }, [messageId ]);
+  }, [messageId]);
+
   return chatuser ? (
     <div className='chat-box'>
       <div className="chat-user">
@@ -60,41 +160,37 @@ const Chatbox = () => {
         <img src={assests.help} alt="help" className='help' />
       </div>
       <div className="chat-mssg">
-        <div className="s-mssg">
-          <p className="msg">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quae facilis error, nobis rerum est corrupti eaque quaerat ut earum explicabo assumenda, ipsa vel</p>
-          <div>
-            <img src={assests.profile} alt="" />
-            <p>21:47 PM</p>
+        {messages.map((msg, index) => (
+          <div key={index} className={msg.sId === userData.id ? "s-mssg" : "r-mssg"}>
+            {msg["image"]
+              ? <img className='msg-img' src={msg.image} alt="img" />
+              : <p className="msg">{msg.text}</p>
+
+            }
+            <div>
+              <img src={msg.sId === userData.id ? userData.avatar : chatuser.userData.avatar} alt="profile" />
+              {console.log(chatuser.userData.avatar,"reciever")}
+              {console.log(userData.avatar,"sender")}
+              <p>{convertTimeStamp(msg.createdAt)}</p>
+            </div>
           </div>
-        </div>
-        <div className="s-mssg">
-          <img src={assests.profile} alt="img" className='msg-img' />          <div>
-            <img src={assests.profile} alt="" />
-            <p>21:47 PM</p>
-          </div>
-        </div>
-        <div className="r-mssg">
-          <p className="msg">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quae facilis error, nobis rerum est corrupti eaque quaerat ut earum explicabo assumenda, ipsa vel</p>
-          <div>
-            <img src={assests.profile} alt="" />
-            <p>21:47 PM</p>
-          </div>
-        </div>
+        ))}
       </div>
       <div className="chat-input">
         <input onChange={(e) => setInput(e.target.value)} value={input} type="text" className="send-a-message" placeholder='Send a message' />
-        <input type="file" id='image' accept='image/png,image/jpeg,image/jpg' hidden />
+        <input onChange={sendImage} type="file" id='image' accept='image/png,image/jpeg,image/jpg' hidden />
         <label htmlFor="image">
           <img src={assests.gallery} alt="gallery" />
         </label>
         <img onClick={sendMessage} src={assests.send} alt="sent" />
       </div>
     </div>
-  )
-    : <div className='chatWelcome'>
+  ) : (
+    <div className='chatWelcome'>
       <img src={assests.logo} alt="logo" />
       <p>Chat as you want!!</p>
     </div>
-}
+  );
+};
 
-export default Chatbox
+export default Chatbox;
